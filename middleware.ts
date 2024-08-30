6f90c4c6248203d171acda042072ae1ef1fd813f
Aditya <adityasahoo652@gmail.com>
2024-08-30 14:43:59 +0530

    refactor: Update Clerk middleware configuration matcher

--- middleware.ts
+++ middleware.ts
@@ -7,8 +7,5 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
