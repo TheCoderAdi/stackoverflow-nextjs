ae8f114870e2677d237e7ff365baa919275fe07d
Aditya <adityasahoo652@gmail.com>
2024-08-30 14:54:35 +0530

    fix:/ image issue

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -53,7 +53,7 @@ const QuestionCard = ({
       </div>
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl="/assets/icons/avatar.svg"
+          imgUrl={author.picture}
           alt="User"
           value={author.name}
           title={` - asked ${getTimestamp(createdAt)}`}
