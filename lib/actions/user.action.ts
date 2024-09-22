8c152d0b8d84ef3a5e49d564e48f130d07f05f53
Aditya <adityasahoo652@gmail.com>
2024-09-22 16:36:05 +0530

    feat: Update sorting criteria for user questions in getUserQuestions function

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -210,7 +210,7 @@ export async function getUserQuestions(params: GetUserStatsParams) {
     const userQuestions = await Question.find({ author: userId })
       .skip(skipAmount)
       .limit(pageSize)
-      .sort({ views: -1, upvotes: -1 })
+      .sort({ createdAt: -1, views: -1, upvotes: -1 })
       .populate("tags", "_id name")
       .populate("author", "_id clerkId name picture");
 
