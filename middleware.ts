10ab758a009663864dee2c127521b1919459c0a9
Aditya <adityasahoo652@gmail.com>
2024-10-01 20:45:55 +0530

    feat: Update middleware to protect "/collection" route

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtected = createRouteMatcher(["/ask-question/(.*)"]);
+const isProtected = createRouteMatcher(["/ask-question/(.*)", "/collection"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtected(req)) auth().protect();
